33fe76afe5bd546a7c45dd0c195e853e182e20ef
Vafo Tagoyev <muslim.whitehacker@gmail.com>
2022-12-26 12:25:03 +0500

    initial commit

---
+++ Pet-Projects/EmployeeManagementSystem/src/main/java/com/whitehacker/springboot/model/Employee.java
@@ -0,0 +1,75 @@
+package com.whitehacker.springboot.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id")
+    private String emailId;
+
+    @Column(name = "age")
+    private int age;
+
+    public Employee() {
+
+    }
+
+    public Employee(String firstName, String lastName, String emailId, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
